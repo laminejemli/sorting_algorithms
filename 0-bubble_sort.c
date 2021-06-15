c370b7fe51a27a362c90556e53e6d88e3b33789c
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2021-06-15 11:00:46 +0000

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,9 +10,9 @@ void bubble_sort(int *array, size_t size)
 	size_t step, i;
 	int temp;
 
-		for  (step = 0; step < size - 1; ++step)
+		for  (step = 0; step < size - 1; step++)
 		{
-			for (i = 0; i < size - step - 1; ++i)
+			for (i = 0; i < size - step - 1; i++)
 			{
 				if (array[i] > array[i + 1])
 				{
